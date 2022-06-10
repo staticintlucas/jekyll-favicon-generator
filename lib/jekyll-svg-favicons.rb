62b94b10e95fd3f3d9cede6d41dcdc048f1a4fb8
Lucas <7199136+staticintlucas@users.noreply.github.com>
2022-06-10 14:40:41 +0100

    Fix missing error function

--- lib/jekyll-svg-favicons.rb
+++ lib/jekyll-svg-favicons.rb
@@ -8,19 +8,25 @@ require "jekyll-svg-favicons/hook"
 module JekyllSvgFavicons
   autoload :VERSION, "jekyll-svg-favicons/version"
 
-  def debug(topic = "SVG Favicons:", msg) # rubocop:disable Style/OptionalArguments
+  LOGGER_PREFIX = "SVG Favicons:"
+
+  def debug(topic = LOGGER_PREFIX, msg) # rubocop:disable Style/OptionalArguments
     Jekyll.logger.debug topic, msg
   end
 
-  def info(topic = "SVG Favicons:", msg) # rubocop:disable Style/OptionalArguments
+  def info(topic = LOGGER_PREFIX, msg) # rubocop:disable Style/OptionalArguments
     Jekyll.logger.info topic, msg
   end
 
-  def warn(topic = "SVG Favicons:", msg) # rubocop:disable Style/OptionalArguments
+  def warn(topic = LOGGER_PREFIX, msg) # rubocop:disable Style/OptionalArguments
     Jekyll.logger.warn topic, msg
   end
 
-  def abort_with(topic = "SVG Favicons:", msg) # rubocop:disable Style/OptionalArguments
+  def error(topic = LOGGER_PREFIX, msg) # rubocop:disable Style/OptionalArguments
+    Jekyll.logger.error topic, msg
+  end
+
+  def abort_with(topic = LOGGER_PREFIX, msg) # rubocop:disable Style/OptionalArguments
     Jekyll.logger.abort_with topic, msg
   end
 end
